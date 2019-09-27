5dee6bfdf8592e4dfec0c6bce56b971b8ad8b74f
ainesh1998 <ainesh1998@outlook.com>
2019-09-27 07:23:47 +0100

    basic email-sending functionality added

--- comp-notification.py
+++ comp-notification.py
@@ -1,5 +1,13 @@
-import requests
+import requests, smtplib, ssl
 from bs4 import BeautifulSoup
+from string import Template
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def read_template(filename):
+    with open(filename, 'r', encoding='utf-8') as template_file:
+        template_file_content = template_file.read()
+    return Template(template_file_content)
 
 wantedLocations = ["Malaysia", "United Kingdom"]
 url = "https://www.worldcubeassociation.org/competitions"
@@ -13,3 +21,26 @@ for i in range(len(locations)):
     for j in wantedLocations:
         if j in countries[i]:
             print(comps[i])
+
+MY_ADDRESS = 'ainesh1998@outlook.com'
+PASSWORD = input("Type your password and press enter: ")
+
+s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
+s.starttls()
+s.login(MY_ADDRESS, PASSWORD)
+
+message_template = read_template('message.txt')
+
+msg = MIMEMultipart()
+message = message_template.substitute(COMP="Beacon House Open 2019")
+
+msg['From']=MY_ADDRESS
+msg['To']="ainesh1998@outlook.com"
+msg['Subject']="New WCA Competition Announced"
+
+msg.attach(MIMEText(message, 'plain'))
+
+s.send_message(msg)
+del msg
+
+s.quit()
