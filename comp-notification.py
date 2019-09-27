9ffd54f7d4cf2c2daf3628b52c4bcf0ef92c89b2
ainesh1998 <ainesh1998@outlook.com>
2019-09-27 08:33:20 +0100

    comps already found are now stored in a dictionary

--- comp-notification.py
+++ comp-notification.py
@@ -1,4 +1,4 @@
-import requests, smtplib, ssl
+import requests, smtplib, ssl, schedule
 from bs4 import BeautifulSoup
 from string import Template
 from email.mime.multipart import MIMEMultipart
@@ -11,36 +11,48 @@ def read_template(filename):
 
 wantedLocations = ["Malaysia", "United Kingdom"]
 url = "https://www.worldcubeassociation.org/competitions"
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'html.parser')
-locations = soup.findAll("div", {"class": "location"})
-countries = [location.strong for location in locations]
-comps = soup.findAll("div", {"class": "competition-link"})
-
-for i in range(len(locations)):
-    for j in wantedLocations:
-        if j in countries[i]:
-            print(comps[i])
-
 MY_ADDRESS = 'ainesh1998@outlook.com'
-PASSWORD = input("Type your password and press enter: ")
+# PASSWORD = input("Type your password and press enter: ")
+compsFound = {}
 
-s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
-s.starttls()
-s.login(MY_ADDRESS, PASSWORD)
+def getComps():
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    locations = soup.findAll("div", {"class": "location"})
+    countries = [location.strong.text.strip() for location in locations]
+    allComps = soup.findAll("div", {"class": "competition-link"})
 
-message_template = read_template('message.txt')
+    for i in range(len(locations)):
+        for j in wantedLocations:
+            if j in countries[i]:
+                linkTag = allComps[i].a
+                name = linkTag.text.strip()
+                link = "https://worldcubeassociation.org/" + linkTag.get('href')
+                compsFound[name] = link
 
-msg = MIMEMultipart()
-message = message_template.substitute(COMP="Beacon House Open 2019")
+# s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
+# s.starttls()
+# s.login(MY_ADDRESS, PASSWORD)
+#
+# message_template = read_template('message.txt')
+#
+# msg = MIMEMultipart()
+# message = message_template.substitute(COMP="Beacon House Open 2019")
+#
+# msg['From']=MY_ADDRESS
+# msg['To']="ainesh1998@outlook.com"
+# msg['Subject']="New WCA Competition Announced"
+#
+# msg.attach(MIMEText(message, 'plain'))
+#
+# s.send_message(msg)
+# del msg
+#
+# s.quit()
 
-msg['From']=MY_ADDRESS
-msg['To']="ainesh1998@outlook.com"
-msg['Subject']="New WCA Competition Announced"
+def main():
+    getComps()
 
-msg.attach(MIMEText(message, 'plain'))
 
-s.send_message(msg)
-del msg
-
-s.quit()
+if __name__ == "__main__":
+    main()
